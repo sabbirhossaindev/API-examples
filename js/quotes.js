61183541fbd96f5742c83541b9cf91f00abb0136
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-28 15:37:05 +0600

    keyne explore

--- js/quotes.js
+++ js/quotes.js
@@ -2,10 +2,10 @@ const loadQuote = () => {
     fetch('https://api.kanye.rest/')
         .then(res => res.json())
         .then(data => displayQuote(data))
-}
+};
 
 const displayQuote = quote => {
     const blockQuote = document.getElementById('doute');
     console.log(quote);
     blockQuote.innerText = quote.quote;
-}
+};

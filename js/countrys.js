cfc129aa4e169c3e311ca929a5c1ec2e76ae3606
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-28 16:24:04 +0600

    country explore

--- js/countrys.js
+++ js/countrys.js
@@ -4,40 +4,34 @@ const loadCountries = () =>{
     .then(data => displayCountries(data));
 }
 
-const displayCountries = countries =>{
-    // for(const country of countries){
-    //     console.log(country);
-    // }
+const displayCountries = countries => {
     const countriesContainer = document.getElementById('countries-container');
     countries.forEach(country => {
         const countryDiv = document.createElement('div');
         countryDiv.classList.add('country');
-        // console.log(country);
         countryDiv.innerHTML = `
             <h3>Name: ${country.name.common}</h3>
             <p>Capital: ${country.capital ? country.capital[0] : 'No Capital'}</p>
             <button onclick="loadCountryDetail('${country.cca2}')">Details</button>
         `;
         countriesContainer.appendChild(countryDiv);
-    })
-}
+    });
+};
 
 const loadCountryDetail = (code) => {
-    // https://restcountries.com/v3.1/alpha/{code}
     const url = `https://restcountries.com/v3.1/alpha/${code}`
-    // console.log('get country detail', code)
     fetch(url)
-    .then(res => res.json())
-    .then(data => displayCountryDetail(data[0]))
-}
+        .then(res => res.json())
+        .then(data => displayCountryDetail(data[0]))
+};
 
-const displayCountryDetail = country =>{
+const displayCountryDetail = country => {
     console.log(country);
     const countryDetail = document.getElementById('country-detail');
     countryDetail.innerHTML = `
-        <h2>Details: ${country.name.common}</h2>
+        <h2>Country Name: ${country.name.common}</h2>
         <img src="${country.flags.png}">
     `
-}
+};
 
-loadCountries()
+loadCountries();

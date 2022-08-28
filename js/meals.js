c666072acce0244dca0ce7bd44176bda73c21fdc
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-28 14:39:21 +0600

    country explore

---
+++ js/meals.js
@@ -0,0 +1,62 @@
+// search Field & display Meals
+const loadMeals = (search) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayMeals(data.meals));
+}
+
+// display Meals
+const displayMeals = meals => {
+    const mealsContainer = document.getElementById('meal-container');
+    mealsContainer.innerHTML = ``;
+    meals.forEach(meal => {
+        // console.log(meal);
+        const mealDiv = document.createElement('div');
+        mealDiv.classList.add('col');
+        mealDiv.innerHTML = `
+        <div onclick="loadMealDetail(${meal.idMeal})" class="card">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${meal.strMeal}</h5>
+            <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+            </div>
+        </div>
+        `;
+        mealsContainer.appendChild(mealDiv);
+    })
+};
+
+// search Field
+const searchFood = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadMeals(searchText);
+    searchField.value = '';
+};
+
+// Meal Details
+const loadMealDetail = (idMeal) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealsDetail(data.meals[0]))
+};
+
+// display Meals detail
+const displayMealsDetail = meal => {
+    const detailContainer = document.getElementById('detail-container');
+    detailContainer.innerHTML = '';
+    const mealDiv = document.createElement('div');
+    mealDiv.classList.add('card');
+    mealDiv.innerHTML = `
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${meal.strMeal}</h5>
+        <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+    </div>
+`;
+    detailContainer.appendChild(mealDiv);
+}
+
+loadMeals('');
